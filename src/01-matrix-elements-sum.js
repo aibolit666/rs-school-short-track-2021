e79936e5b63001ed0fe7521b92128c380f367fea
Pavel <pavel.priladyshev@gmail.com>
2021-04-09 03:27:27 +0300

    init: Start task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,28 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0 && typeof matrix[i + 1] !== 'undefined') {
+        matrix[i + 1][j] = 0;
+      }
+    }
+  }
+
+  const result = [];
+  for (let k = 0; k < matrix.length; k++) {
+    for (let n = 0; n < matrix[0].length; n++) {
+      if (matrix[k][n] !== 0) {
+        result.push(matrix[k][n]);
+      }
+    }
+  }
+  let summa = 0;
+  for (let s = 0; s < result.length; s++) {
+    summa += result[s];
+  }
+  return summa;
 }
 
 module.exports = getMatrixElementsSum;
